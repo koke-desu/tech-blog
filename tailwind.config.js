7c9a4bbae5fb6514e079d5be055dbbea2ed06f1f
koke-desu <gbpvp25645@yahoo.co.jp>
2023-03-04 10:22:20 +0900

    feat: Add tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      "p-blue": "#9ADCFF",
+      "p-yellow": "#FFF89A",
+      "p-orange": "#FFB2A6",
+      "p-pink": "#FF8AAE",
+      text: "#414141",
+      bg: "#F8F4F3",
+    },
   },
   plugins: [],
 };
