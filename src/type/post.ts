ea0077b320cd4de1dd8ec6465fcdc5846210b252
koke-desu <gbpvp25645@yahoo.co.jp>
2023-03-28 10:03:36 +0900

    feat: Define PostBody type

--- src/type/post.ts
+++ src/type/post.ts
@@ -1,11 +1,20 @@
 import { Dayjs } from "dayjs";
 import { TagType } from "./tag";
 
+// すでにParseしてDBに保存されているデータの型
+export type PostBodyType =
+  | {
+      heading: string;
+      level: number;
+      children: PostBodyType[];
+    }
+  | string;
+
 export type PostType = {
   id: string;
   title: string;
   tags: TagType[];
   createdAt: Dayjs;
   updatedAt: Dayjs;
-  body: string;
+  body: PostBodyType[];
 };
