49c87c374e8eef4661c6fe776f8842c57eaa92ea
koke-desu <gbpvp25645@yahoo.co.jp>
2023-04-15 15:50:17 +0900

    feat: Make Tag smaller

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -5,7 +5,7 @@ type Props = { tag: TagType };
 
 const Tag: React.FC<Props> = ({ tag }) => {
   return (
-    <div className="flex flex-row gap-3 px-4 py-2 border border-text rounded-2xl max-w-max">
+    <div className="flex flex-row gap-2 px-2 py-1 border border-gray rounded-2xl max-w-max">
       <Image src={tag.icon} alt={tag.label} width={24} height={24} />
       <p className="text-base text-text font-bold">{tag.label}</p>
     </div>
