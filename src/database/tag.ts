0f6f6222b4e16337a9a4ae3c5dc61c84dd6aa8d3
koke-desu <gbpvp25645@yahoo.co.jp>
2023-07-20 00:38:57 +0900

    feat: Fix to pure get function

--- src/database/tag.ts
+++ src/database/tag.ts
@@ -1,19 +1,12 @@
 import { TagType } from "@/src/type/tag";
-import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import db from "./initialize";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 // 記事のデータ一覧を取得する。
-export const useGetTags = async (): Promise<TagType[]> => {
-  const [tags, setTags] = useState<TagType[]>([]);
-  useEffect(() => {
-    getDocs(collection(db, "tags")).then((docs) => {
-      const tags = docs.docs.map((doc) => {
-        return doc.data() as TagType;
-      });
-      setTags(tags);
+export const getTags = async (): Promise<TagType[]> => {
+  const db = getFirestore();
+  return getDocs(collection(db, "tags")).then((docs) => {
+    return docs.docs.map((doc) => {
+      return doc.data() as TagType;
     });
-  }, []);
-
-  return [];
+  });
 };
