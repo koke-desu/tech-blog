2d2b83cc94d039d0899051204a5b56ce6085ce3b
koke-desu <gbpvp25645@yahoo.co.jp>
2023-07-22 23:22:33 +0900

    feat: Add image domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
